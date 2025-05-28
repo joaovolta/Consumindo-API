d6cf4c9e3c8e9c24c9314bde4404224c2a61392b
joaovolta <joaovolta04@gmail.com>
2025-05-28 16:38:08 -0300

    Inicio projeto: Consumindo API e aprendendo a trabalhar com os dados

---
+++ ScreenSound/Modelos/Musica.cs
@@ -0,0 +1,25 @@
+using System.Text.Json.Serialization;
+
+namespace ScreenSound.Modelos;
+
+internal class Musica
+{
+    // JsonPropertyName é usado para mapear as propriedades do JSON para as propriedades da classe
+    // Isso é necessário quando os nomes das propriedades no JSON não correspondem exatamente aos nomes das propriedades na classe
+    [JsonPropertyName("song")]
+    public string? Nome { get; set; } // ? significa que a propriedade pode ser nula
+    [JsonPropertyName("artist")]
+    public string? Artista { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+    [JsonPropertyName("genre")]
+    public string? Genero { get; set; }
+
+    public void ExibirDetalhes()
+    {
+        System.Console.WriteLine($"Nome: {Nome}");
+        System.Console.WriteLine($"Artista: {Artista}");
+        System.Console.WriteLine($"Duração: {Duracao} ms");
+        System.Console.WriteLine($"Gênero: {Genero}");
+    }
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.WebSockets;
+using System.Text.Json;
+using ScreenSound.Modelos;
+
+using (HttpClient client = new())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+        System.Console.WriteLine(resposta);
+        var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!; // JsonSerializer é usado para desserializar o JSON em uma lista de objetos Musica
+        musicas[0].ExibirDetalhes(); // Exibe os detalhes da primeira música
+    }
+    catch (Exception ex)
+    {
+        System.Console.WriteLine($"Erro ao obter a resposta: {ex.Message}");
+    }
+}
